514bdafb1584c8cfe104549a5138b61ea8dcc829
HIHJH <2170095@ewhain.net>
2024-06-24 18:15:01 +0900

    feat: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from './App.tsx'
 import './index.css'
 import '@fonts/fonts.css'
 import GlobalStyles from './styles/GlobalStyles.ts'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyles />
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
